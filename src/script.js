e37f90f8536f848ac09816459582e4fceeb9019f
Rahiq Al-Makhtum <rahiq789@gmail.com>
2025-10-03 17:03:30 +0600

    added image count functionality

--- src/script.js
+++ src/script.js
@@ -1,7 +1,46 @@
+const body = document.body;
 const toggleNightMode = document.getElementById("toggleNight");
-const body = document.querySelector("body");
+const mainSection = document.querySelector("section.bg-gradient-custom");
+const promtForm = document.getElementById("promt-form");
+const imageGrid = document.querySelectorAll(".imageGrid");
+const imageCount = document.getElementById("image-count")
 
-toggleNightMode.addEventListener("click", () => {
-  body.classList.toggle("bg-gradient-custom"); // day gradient
-  body.classList.toggle("bg-black"); // night background
+toggleNightMode.addEventListener("click", function() {
+console.log("Toggle clicked!");
+        
+// Toggle body background
+body.classList.toggle("bg-gradient-custom");
+body.classList.toggle("bg-gradient-custom-night");
+        
+// Toggle section background
+
+mainSection.classList.toggle("bg-gradient-custom");
+mainSection.classList.toggle("bg-gradient-custom-night");
+
 });
+
+function renderImage(count){
+    if (count == 1) {
+      imageGrid.className = "grid grid-cols-1 gap-4 mt-6";
+    } else if (count == 2) {
+      imageGrid.className = "grid grid-cols-2 gap-4 mt-6";
+    } else if (count == 3) {
+      imageGrid.className = "grid grid-cols-3 gap-4 mt-6";
+    } else {
+      imageGrid.className = "grid grid-cols-1 md:grid-cols-4 gap-4 mt-6";
+    }
+    imageGrid.forEach((item, index) => {
+        if(index <= count){
+            item.classList.remove("hidden"); // show
+        } else {
+            item.classList.add("hidden"); // hide
+        }
+    });
+}
+
+promtForm.addEventListener("submit", (e) => {
+   e.preventDefault();
+    const count = parseInt(imageCount.value);
+    if(!count) return; // do nothing if not selected
+    renderImage(count);
+})
